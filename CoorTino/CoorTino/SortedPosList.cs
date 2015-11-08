de998bea8786e46649fd022e3655d376eb0035cb
MatsDrevstad <mats.drevstad@gmail.com>
2015-11-08 10:04:25 +0100

    remove

--- CoorTino/CoorTino/SortedPosList.cs
+++ CoorTino/CoorTino/SortedPosList.cs
@@ -23,6 +23,13 @@ namespace CoorTino
 
         public bool Remove(Position pos)
         {
+            var element = positions
+                .FirstOrDefault(p => p.Equals(pos));
+            if (element != null)
+            {
+                positions.Remove(element);
+                return true;
+            }
             return false;
         }
     }
