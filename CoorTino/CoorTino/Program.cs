aca292a5bd933af53471ebf62b761fe1872b5dc2
MatsDrevstad <mats.drevstad@gmail.com>
2015-11-08 15:37:15 +0100

    sortedposlist

--- CoorTino/CoorTino/Program.cs
+++ CoorTino/CoorTino/Program.cs
@@ -22,20 +22,20 @@ namespace CoorTino
             list1.Add(new Position(2, 6));
             list1.Add(new Position(2, 3));
             Console.WriteLine(list1 + "\n");
-            //list1.Remove(new Position(2, 6));
-            //Console.WriteLine(list1 + "\n");
+            list1.Remove(new Position(2, 6));
+            Console.WriteLine(list1 + "\n");
 
-            //list2.Add(new Position(3, 7));
-            //list2.Add(new Position(1, 2));
-            //list2.Add(new Position(3, 6));
-            //list2.Add(new Position(2, 3));
-            //Console.WriteLine((list2 + list1) + "\n");
+            list2.Add(new Position(3, 7));
+            list2.Add(new Position(1, 2));
+            list2.Add(new Position(3, 6));
+            list2.Add(new Position(2, 3));
+            Console.WriteLine((list2 + list1) + "\n");
 
-            //SortedPosList circleList = new SortedPosList();
-            //circleList.Add(new Position(1, 1));
-            //circleList.Add(new Position(2, 2));
-            //circleList.Add(new Position(3, 3));
-            //Console.WriteLine(circleList.CircleContent(new Position(5, 5), 4) + "\n");
+            SortedPosList circleList = new SortedPosList();
+            circleList.Add(new Position(1, 1));
+            circleList.Add(new Position(2, 2));
+            circleList.Add(new Position(3, 3));
+            Console.WriteLine(circleList.CircleContent(new Position(5, 5), 4) + "\n");
 
             /*VG uppgift test
             Console.WriteLine((list2 - list1) + "\n");
--- CoorTino/CoorTino/SortedPosList.cs
+++ CoorTino/CoorTino/SortedPosList.cs
@@ -8,26 +8,76 @@ namespace CoorTino
 {
     class SortedPosList
     {
-        private List<Position> positions = new List<Position>();
+        private List<Position> positionList = new List<Position>();
+
+        public SortedPosList() {}
+
+        public SortedPosList(SortedPosList spl)
+        {
+            positionList = spl.positionList;
+        }
 
         public int Count()
         {
-            return positions.Count();
+            return positionList.Count();
         }
 
         public void Add(Position pos)
         {
-            positions.Add(pos);
-            positions.OrderBy(p => p.Lenght()).ToList();
+            positionList.Add(pos);
+            positionList = positionList
+                .OrderBy(p => p.Lenght()).ToList();
         }
 
         public bool Remove(Position pos)
         {
-            var element = positions
+            var element = positionList
                 .FirstOrDefault(p => p.Equals(pos));
             if (element != null)
             {
-                positions.Remove(element);
+                positionList.Remove(element);
+                return true;
+            }
+            return false;
+        }
+
+        override public string ToString()
+        {
+            return string.Join(", ", positionList);
+        }
+
+        public SortedPosList Clone()
+        {
+            return new SortedPosList(this);
+        }
+
+        public static SortedPosList operator +(SortedPosList sp1, SortedPosList sp2)
+        {
+            foreach (var item in sp2.positionList)
+            {
+                sp1.Add(item);
+            }
+            return sp1.Clone();
+        }
+
+        public SortedPosList CircleContent(Position centerPos, double radius)
+        {
+            var temp = new SortedPosList();
+
+            foreach (var item in positionList)
+            {
+                if (IsInside(item, centerPos, radius))
+                {
+                    temp.Add(item);
+                }
+            }
+            return temp;
+        }
+
+        private bool IsInside(Position pos, Position centerPos, double radius)
+        {
+            if (Math.Pow((pos.X - centerPos.X), 2) + Math.Pow((pos.Y- centerPos.Y), 2) < Math.Pow(radius, 2))
+            {
                 return true;
             }
             return false;
